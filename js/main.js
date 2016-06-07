fe7157741a6e1626c7cd029e0e785182b271dfcb
Senad Licina <sli@sli-macbook.starfinanz.de>
2016-06-07 18:03:03 +0200

    now using materializeCSS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-$('#background').enllax();
+$(document).ready(function () {
+  $('#background').enllax();
+  
+  $(".button-collapse").sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - $("#navbar").outerHeight(true) - 50
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  //jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+      $('.page-scroll').bind('click', function(event) {
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollTop: $($anchor.attr('href')).offset().top - $("#navbar").outerHeight(true) - 50
+          }, 1500, 'easeInOutExpo');
+          event.preventDefault();
+      });
+  });
 });
